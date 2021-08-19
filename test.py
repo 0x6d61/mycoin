086c20dd2a3c754ec24bc54f9f2205da09841aed
0x6d61 <0x6d61@gmail.com>
2021-08-19 22:58:05 +0900

    スカラー倍算を実装

--- test.py
+++ test.py
@@ -1,15 +1,23 @@
+import unittest
 from ecc import FieldElement,Point
+from unittest import TestCase
 
-prime = 223
+class ECCTest(TestCase):
+    def test_on_curve(self):
+        prime = 223
+        a = FieldElement(0,prime)
+        b = FieldElement(7,prime)
+        valid_points = ((192,105),(17,56),(1,193))
+        invalid_points = ((200,119),(42,99))
 
-a = FieldElement(0,prime)
-b = FieldElement(7,prime)
-x1 = FieldElement(192,prime)
-y1 = FieldElement(105,prime)
-x2 = FieldElement(17,prime)
-y2 = FieldElement(56,prime)
+        for x_raw,y_raw in valid_points:
+            x = FieldElement(x_raw,prime)
+            y = FieldElement(y_raw,prime)
+            Point(x,y,a,b)
+        for x_raw,y_raw in invalid_points:
+            x = FieldElement(x_raw,prime)
+            y = FieldElement(y_raw,prime)
+            with self.assertRaises(ValueError):
+                Point(x,y,a,b)
 
-p1 = Point(x1,y1,a,b)
-p2 = Point(x2,y2,a,b)
-
-print(p1+p2)
+unittest.main()
